534d845099fb041c84ebd606a438c2b7700624d0
its0su0rj <155565040+its0su0rj@users.noreply.github.com>
2024-09-08 13:37:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def load_questions(file_url):
 def get_quiz_sets():
     # Replace this list with actual URLs to your CSV files in the GitHub repo
     quiz_sets = {
-        "Set 1": "https://raw.githubusercontent.com/its0su0rj/owspracticeits-suraj/main/ca4_set1.csv",
+         "Set 1": "https://raw.githubusercontent.com/its0su0rj/owspracticeits-suraj/main/ca4_set1.csv",
         "Set 2": "https://raw.githubusercontent.com/its0su0rj/owspracticeits-suraj/main/ca4_set2.csv",
         "Set 3": "https://raw.githubusercontent.com/its0su0rj/owspracticeits-suraj/main/ca4_set3.csv",
         "Set 4": "https://raw.githubusercontent.com/its0su0rj/owspracticeits-suraj/main/ca4_set4.csv"
@@ -70,7 +70,7 @@ def main():
             # Check answers and calculate score
             incorrect_answers = []
             for i, row in df.iterrows():
-                correct_option = row[row['correct_ans']]
+                correct_option = row[row['correct_ans']]  # Correct answer column value
                 if user_answers[i] == correct_option:
                     score += 1
                 else:
